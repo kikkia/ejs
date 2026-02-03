124d6d81a502104c3a5ed642e24003d025b2a8ef
Simon Sawicki <contact@grub4k.dev>
2026-02-03 12:02:54 +0100

    Allow download from web using extract.ts

--- src/yt/solver/extract.ts
+++ src/yt/solver/extract.ts
@@ -1,11 +1,15 @@
 import { parse } from "meriyah";
 import { getIO } from "./test/io.ts";
+import { downloadCached } from "./test/utils.ts";
 import { argv } from "node:process";
 import { getSolutions, modifyPlayer } from "./solvers.ts";
 import { generate } from "astring";
 
-const io = await getIO();
-const data = await io.read(argv[2]);
+const data = await (argv.length > 3 ? (() => downloadCached(argv[2], argv[3])) : (async () => {
+  const io = await getIO();
+  return await io.read(argv[2]);
+}))();
+
 const program = parse(data);
 const statements = modifyPlayer(program);
 const solutionMap = getSolutions(statements);
